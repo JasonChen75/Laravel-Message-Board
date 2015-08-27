29d8ab039d14cb418787ef5d15189d5271c378c7
Tina <tina84318@yahoo.com.tw>
2015-08-27 16:49:01 +0800

    #5 add database for message saving

--- laravel/database/migrations/2015_08_27_082906_create_saveComtent_table.php
+++ laravel/database/migrations/2015_08_27_082906_create_saveComtent_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSaveComtentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
         Schema::create('MessageBoard', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('name') ;
-            $table->string('content') ;
+            $table->string('name');
+            $table->string('content');
         });
     }
 
